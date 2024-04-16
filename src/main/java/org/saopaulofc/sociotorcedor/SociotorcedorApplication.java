af3032e9c15126139906f0dbd3bd0860ead56a6a
Patrick Rehder <patrickrehderbosco@hotmail.com>
2024-04-16 16:43:44 -0300

    Corrigindo erro de CORS

--- src/main/java/org/saopaulofc/sociotorcedor/SociotorcedorApplication.java
+++ src/main/java/org/saopaulofc/sociotorcedor/SociotorcedorApplication.java
@@ -1,9 +1,11 @@
 package org.saopaulofc.sociotorcedor;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @EnableFeignClients
 @SpringBootApplication
 public class SociotorcedorApplication {
